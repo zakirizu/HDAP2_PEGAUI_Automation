e12e259c2be218cc6525bdc9c4cc27561bac0be0
Zakir Hussain Shaik <zakir.13.2011@gmail.com>
2025-02-11 17:13:18 +0530

    Update the code

--- src/main/java/utils_API/Automated_TestData_Creator.java
+++ src/main/java/utils_API/Automated_TestData_Creator.java
@@ -8,7 +8,7 @@ import factory.Constants;
 import utils.PropertiesFileReader;
 
 public class Automated_TestData_Creator {
-	 private static final String env = "UAT";
+	 private static final String env = "QA";
 ;
 	
 	    
@@ -21,8 +21,9 @@ public class Automated_TestData_Creator {
 	        	{
 	        	//Enter the Request Group and Provider Details Here for QA Environment
 	        	//Valid Set of Data Do Not Delete this. 
-	        	//	{"RG-20002", "P-388412033222"},
-	        		{"RG-20013","P-329988776655"},
+	        	//Updated the code
+	        		{"RG-20002", "P-388412033222"},
+	        	//	{"RG-20013","P-329988776655"},
 	        		//{"RG-22001","P-388412068877"},
 	        	//Invalid Set of Data 	
 	        		//{"RG-22001","P-329988776655"},
@@ -43,7 +44,7 @@ public class Automated_TestData_Creator {
 	    }
 
 		
-	@Test(dataProvider = env , invocationCount = 3)
+	@Test(dataProvider = env , invocationCount = 15)
 	public void processDataAndGenerateOutput(String requestGroup, String providerId) {	
 			System.out.println();
 			System.out.println(COLORS.GREEN+"Creating the Test Data in " +COLORS.RED+env+COLORS.GREEN+" Environment. If you want to change the Environment, Kindly stop and update the 'Env' Variable Under the API Properiets File"+COLORS.RESET);
